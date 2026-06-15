ec460b770e8b0b8371650363dcf3ad6d7a4ba84a
ajuavinett <ajuavine@ucsd.edu>
2026-06-15 14:03:17 -0700

    original files

---
+++ HierarchicalBootstrap/hierarchical_bootstrap.py
@@ -0,0 +1,89 @@
+"""
+Wrapper function for hierarchical bootstrap tests.
+"""
+
+# imports
+from twosample_hierarchical_bootstrap import twosample_hierarchical_bootstrap
+from paired_hierarchical_bootstrap import paired_hierarchical_bootstrap
+from onesample_hierarchical_bootstrap import onesample_hierarchical_bootstrap
+from utils import check_input
+
+
+def hierarchical_bootstrap(df, variable, level_1, level_2, condition=None,
+                           paired=False, one_sample=False, n_iterations=1000, 
+                           verbose=True, plot=True):
+    """
+    Wrapper function for hierarchical bootstrap tests.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        Dataframe containing the data.
+    variable : str
+        Name of the variable to test.
+    level_1 : str
+        First level of hierarchy to resample. This is the higher level i.e.
+        level_2 is nested within level_1; e.g. electrodes within subjects.
+    level_2 : str
+        Second level of hierarchy to resample. This is the lower level i.e.
+        level_2 is nested within level_1; e.g. electrodes within subjects.
+    condition : str, optional
+        Name of the condition to test. Required for two-sample tests.
+        Default is None.
+    paired : bool, optional
+        Whether to perform a paired test. Default is False.
+    one_sample : bool, optional
+        Whether to perform a one-sample test. Default is False.
+    n_iterations : int, optional
+        Number of bootstrap iterations. Default is 1000.
+    verbose : bool, optional
+        Whether to print results. Default is True.
+    plot : bool, optional
+        Whether to plot results. Default is True.
+
+    Returns
+    -------
+    dict
+        Dictionary containing the results of the test.
+
+    Usage
+    -----
+    # two-sample test
+    hierarchical_bootstrap(df, variable, level_1, level_2, condition=None,
+                          paired=False, one_sample=False, n_iterations=1000, 
+                          verbose=True, plot=True
+
+    # paired two-sample test
+    hierarchical_bootstrap(df, variable, level_1, level_2, condition=None,
+                          paired=True, one_sample=False, n_iterations=1000, 
+                          verbose=True, plot=True
+
+    # one-sample test
+    hierarchical_bootstrap(df, variable, level_1, level_2, condition=None,
+                          paired=False, one_sample=True, n_iterations=1000, 
+                          verbose=True, plot=True
+    """
+    # check input data
+    df = check_input(df, variable, level_1, level_2, condition)
+
+    # One-sample test
+    if one_sample:
+        return onesample_hierarchical_bootstrap(df, variable, level_1, level_2,
+                                                n_iterations, verbose, plot)
+
+    # Two-sample tests
+    else:
+        # check input data
+        if condition is None:
+            raise ValueError("Condition must be specified for two-sample test.")
+
+        if paired:
+            # paired two-sample test
+            return paired_hierarchical_bootstrap(df, variable, condition, 
+                                                 level_1, level_2, n_iterations,
+                                                 verbose, plot)
+        else:
+            # two-sample test
+            return twosample_hierarchical_bootstrap(df, variable, condition,
+                                                    level_1, level_2, 
+                                                    n_iterations, verbose, plot)
---
+++ README.md
@@ -0,0 +1,14 @@
+# How to use these materials
+
+The slides will open directly on GitHub.
+
+You can interact with the notebooks using the button below:
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](http://colab.research.google.com/github/ajuavinett/SucceedingWithData)
+
+
+# References
+
+* *eLife* [Improving Your Neuroscience series](https://www.eneuro.org/collection/improving-your-neuroscience)
+
+* *Nature* [Points of Significance series](https://www.nature.com/collections/qghhqm/pointsofsignificance)
+
